46499b70d0bd8e06411ee20b086b59a2b5acfe3d
LI HAOLIANG <haoliang.im@gmail.com>
2021-11-27 19:21:25 +0800

    complete 30

---
+++ 30_MinInStack/main30.cpp
@@ -0,0 +1,47 @@
+/*** 
+ * @Date: 2021-11-27 19:04:59
+ * @LastEditors: zjulhl
+ * @LastEditTime: 2021-11-27 19:19:01
+ * @Description: 
+ * @FilePath: /CodingInterviewChinese2/30_MinInStack/main30.cpp
+ */
+#include <iostream>
+#include <limits>
+class MinStack
+{
+private:
+    stack<int> stack_x, stack_min;
+
+public:
+    /** initialize your data structure here. */
+    MinStack()
+    {
+        stack_min.push(INT_MAX);
+    }
+
+    void push(int x)
+    {
+        stack_x.push(x);
+        if (x <= stack_min.top())
+        {
+            stack_min.push(x);
+        }
+    }
+
+    void pop()
+    {
+        if (stack_x.top() == stack_min.top())
+            stack_min.pop();
+        stack_x.pop();
+    }
+
+    int top()
+    {
+        return stack_x.top();
+    }
+
+    int min()
+    {
+        return stack_min.top();
+    }
+};
