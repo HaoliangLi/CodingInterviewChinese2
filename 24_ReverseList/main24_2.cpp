5ee4cc7add086c11abd277e484922170d8f0b97f
LI HAOLIANG <haoliang.im@gmail.com>
2021-11-28 14:37:55 +0800

    fixed 24_2

--- 24_ReverseList/main24_2.cpp
+++ 24_ReverseList/main24_2.cpp
@@ -1,9 +1,11 @@
 /*** 
  * @Date: 2021-11-28 13:56:29
  * @LastEditors: zjulhl
- * @LastEditTime: 2021-11-28 14:25:28
+ * @LastEditTime: 2021-11-28 14:31:56
  * @Description: 
  * @FilePath: /CodingInterviewChinese2/24_ReverseList/main24_2.cpp
+ * 执行用时：8ms，在所有C++提交中击败了 44.85%的用户
+ * 内存消耗：8MB，在所有C++提交中击败了 83.15%的用户
  */
 #include <vector>
 #include <stack>
@@ -26,12 +28,11 @@ public:
         ListNode *cur = head, *pre = nullptr;
         while (cur != nullptr)
         {
-            ListNode *tmp = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = cur->next;
+            ListNode *tmp = cur->next; // 暂存后继节点 cur.next
+            cur->next = pre;           // 修改 next 引用指向
+            pre = cur;                 // pre 暂存 cur
+            cur = tmp;                 // cur 访问下一节点
         }
         return pre;
     }
 };
-
